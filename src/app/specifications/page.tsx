ada9eff1986e4efadd7f74694625df6b00e7c99e
IbrahimChaudhary <ibrahimchaudhary421@gmail.com>
2025-09-26 22:44:11 +0500

    merging specifications page

--- src/app/specifications/page.tsx
+++ src/app/specifications/page.tsx
@@ -1,9 +1,11 @@
+import { DiscountCta } from "@/components/discount-cta";
 import SonicSpecificationsExample from "@/components/sonic-specifications-example";
 
 export default function SpecificationsPage() {
   return (
     <div className="min-h-screen">
       <SonicSpecificationsExample />
+      <DiscountCta/>
     </div>
   );
 }
